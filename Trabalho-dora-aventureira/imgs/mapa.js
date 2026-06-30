dece1e652517461e8b1a8c91a0cf040e0e552cdb
ThauanVitor13 <thauan.esap13@gmail.com>
2026-06-29 22:22:34 -0300

    Update mapa.js

--- Trabalho-dora-aventureira/imgs/mapa.js
+++ Trabalho-dora-aventureira/imgs/mapa.js
@@ -1,31 +1,26 @@
 const botoes =
     document.querySelectorAll(".localizacao");
+    
+function abrirDescricao(B) {
 
-const descricoes =
-    document.querySelectorAll(".descricao");
+    const ativo = B.classList.contains("ativa");
+
+    botoes.forEach((d) => {
+
+        d.classList.remove("ativa");
+
+    });
+
+    if (!ativo) {
+        B.classList.add("ativa");
+    }
+}
 
 botoes.forEach((botao) => {
 
     botao.addEventListener("click", () => {
 
-        const alvo =
-            document.getElementById(
-                botao.dataset.alvo
-            );
-
-        const estaAtivo = alvo.classList.contains("ativa");
-
-        descricoes.forEach((descricao) => {
-
-            descricao.classList.remove("ativa");
-
-        });
-
-        if (!estaAtivo) {
-
-            alvo.classList.add("ativa");
-
-        }
+        abrirDescricao(botao);
 
     });
 
